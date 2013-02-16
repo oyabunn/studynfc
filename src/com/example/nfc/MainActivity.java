0b0ad33015321c8329055503561df38142a9f27d
oyabunn <oyabunnsystems@gmail.com>
2013-02-16 18:44:15 +0900

    writeKeyInfos

--- src/com/example/nfc/MainActivity.java
+++ src/com/example/nfc/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends Activity {
 		byte[] tagid = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
         Tag tag = (Tag)intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         
+        boolean writemode = false;
+        
         int sector = 1;
         if(tag!=null)
         {
@@ -27,17 +29,37 @@ public class MainActivity extends Activity {
         	MifareClassic mfc = MifareClassic.get(tag);
             try {
                 if(!mfc.isConnected())mfc.connect();
-                if(mfc.authenticateSectorWithKeyA(sector,MifareClassic.KEY_DEFAULT)){
-                	byte[] data = new byte[16];
-                	data[0] = (byte)0x00;
-                	data[1] = (byte)0x10;
-                	data[2] = (byte)0xFF;
-                    mfc.writeBlock(sector*4+0,data);
-                    byte[] Block0 = mfc.readBlock(sector*4+0);
-                    String Block0String = stringFromBytes(Block0);
-                    Log.i("nfc","nfc Block0="+Block0String);
+                
+                if(writemode)
+                {
+                	if(mfc.authenticateSectorWithKeyA(sector,MifareClassic.KEY_DEFAULT)){
+                    	byte[] data = new byte[16];
+                    	data[0] = (byte)0x01;
+                    	data[1] = (byte)0x02;
+                    	data[2] = (byte)0x03;
+                        mfc.writeBlock(sector*4+0,data);
+                        
+                        for(int i=0; i<6; i++)data[i] = (byte)(i+1);
+                        data[6] = (byte)0x70;
+                        data[7] = (byte)0xF0;
+                        data[8] = (byte)0xF8;
+                        for(int i=0; i<6; i++)data[i+10] = (byte)(i+1);
+                        mfc.writeBlock(sector*4+3,data);
+                        
+                        Log.i("nfc","nfc written");
+                    }else{
+                        Log.i("nfc","nfc auth fail");
+                    }
                 }else{
-                    Log.i("nfc","nfc auth fail");
+                	byte[] keyB = new byte[16];
+                	for(int i=0; i<6; i++)keyB[i] = (byte)(i+1);
+                	if(mfc.authenticateSectorWithKeyB(sector,keyB)){
+                        byte[] Block0 = mfc.readBlock(sector*4+0);
+                        String Block0String = stringFromBytes(Block0);
+                        Log.i("nfc","nfc Block0="+Block0String);
+                    }else{
+                        Log.i("nfc","nfc auth fail");
+                    }
                 }
                 mfc.close();
             }catch (Exception e)
